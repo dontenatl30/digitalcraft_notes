7fc07bc5fa7a2538a9b1d453cbe4c515d4e21a50
LaDonte LeBlanc <donte.11@hotmail.com>
2023-02-19 19:15:26 -0500

    marinaraSauce_exercise

--- recipe-exercises/script.js
+++ recipe-exercises/script.js
@@ -1,16 +1,19 @@
 
-function marinaraSauce(cookName) {
-    console.log(`Hey ${cookName} this is my marinaraSauce receipe`)
-console.log("Heat a medium-large saucepan over medium heat.");
-console.log();
-console.log ("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.");
-console.log();
-console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
-console.log();
-console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.");
-console.log();
-console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
-console.log
+//function marinaraSauce(cookName) {
+  //  console.log(`Hey ${cookName} this is my marinaraSauce receipe`)
+//console.log("Heat a medium-large saucepan over medium heat.");
+//console.log();
+//console.log ("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.");
+//console.log();
+//console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+//console.log();
+//console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.");
+//console.log();
+//console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+//console.log
 }
-marinaraSauce("adam");
+//marinaraSauce("adam");
 
+
+
+marinaraSauce();
