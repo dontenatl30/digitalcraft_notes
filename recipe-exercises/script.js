26328e46e738610542da9eacb144c8e4e0de83ae
LaDonte LeBlanc <donte.11@hotmail.com>
2023-02-21 18:11:54 -0500

    marinara_recipe_completed

--- recipe-exercises/script.js
+++ recipe-exercises/script.js
@@ -1,19 +1,77 @@
+ //function marinaraSauce() {
+   //  console.log("Heat a medium-large saucepan over medium heat.");
+     //console.log();
+     //console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+     //console.log();
+     //console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+     //console.log();
+     //console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+     //console.log();
+     //console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+//}
 
-//function marinaraSauce(cookName) {
-  //  console.log(`Hey ${cookName} this is my marinaraSauce receipe`)
-//console.log("Heat a medium-large saucepan over medium heat.");
-//console.log();
-//console.log ("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.");
-//console.log();
-//console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
-//console.log();
-//console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.");
-//console.log();
-//console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
-//console.log
+//marinaraSauce();
+
+function marinaraSauce(cookName) {
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log();
+    console.log("- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log();
+    console.log("- Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log();
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log();
+    console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
 }
-//marinaraSauce("adam");
-
-
 
 marinaraSauce();
+
+Define the marinaraSauce() function function marinaraSauce(cookName) { 
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!"); 
+console.log("- Heat a medium-large saucepan over medium heat."); 
+console.log(); 
+console.log("- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes"); 
+console.log(); 
+console.log("- Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste."); 
+console.log(); 
+console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes"); 
+console.log(); 
+console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed."); 
+}
+
+function marinaraSauce("Adam");
+
+function marinaraSauce(cookName) {
+    var two = 2;
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log();
+    console.log("- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log();
+    console.log("- Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log();
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log();
+    console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+function marinaraSauce("Adam");
+
+
+function marinaraSauce(cookName) {
+    var two = 2;
+    var oneFourth = "1/4";
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log();
+    console.log("- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + two + " minutes");
+    console.log();
+    console.log("- Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log();
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log();
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+function marinaraSauce("Adam");
+
