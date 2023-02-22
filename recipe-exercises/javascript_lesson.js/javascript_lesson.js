f141c8e075ac568761602d294e44d8af3b213966
LaDonte LeBlanc <donte.11@hotmail.com>
2023-02-21 21:06:41 -0500

    JS_Warm_up_1

---
+++ recipe-exercises/javascript_lesson.js/javascript_lesson.js
@@ -0,0 +1,104 @@
+// const firstName = 'travis';
+// const lastName = 'Ramos';
+// const currentDate = '28';
+// let  programingLanguage = 'Javascript';
+
+// function introduction(firstName, lastName) {
+// const currentMonth = 'February';
+// const currentYear = 2023;
+
+// console.log(`Hello my name is ${firstName} ${lastName}.`);
+// console.log(`i am currently learning how to program in ${programingLanguage}.`);
+// return;
+// programingLanguage = `C++`;
+// console.log(`we changed the program language to ${programingLanguage}.`);
+// }
+
+// introduction(firstName, lastName);
+
+// console.log(currentDate);
+
+//ARRAYS
+
+// const firstArray = ['travis', 'ramos', 'javascript'];
+// const secondArray = [];
+
+// function introduction(ourFirstArray, ourSecondArray) {
+//     ourFirstArray.push('Lobsters');
+//     ourSecondArray.push(55);
+//     console.log(ourFirstArray);
+//     console.log(ourSecondArray);
+//     console.log(`hello! my name is ${ourFirstArray[0] + ' ' + ourFirstArray[1]}.`);
+//     console.log(`i am currently learning how to program in ${ourFirstArray[2]}`);
+//     ourFirstArray[2] = `HTML/CSS`;
+//     console.log(`and i'm learning ${ourFirstArray[2]}`);
+// }
+
+// introduction(firstArray, secondArray);
+
+// console.log(firstArray.length);
+
+// const stringArray = ("HTML", "CSS", "JavaScript", "React", "Bananas")
+// function reIntroduction (stringArray) {
+//     stringArray.pop();
+//     // .splice acts on the array and changes it
+//     // slice returns a new array nd doesnt change the one your slicing.
+//     stringArray.slice();
+//     const newArray = stringArray(0, 3);
+//     stringArray.splice(2, 3);
+// const languages = stringArray.join(' ');
+// console.log(language);
+// }
+
+// reIntroduction(stringArray);
+
+//objects
+
+// const myObject = {
+//     firstName: 'travis',
+//     lastName: 'ramos',
+//     programingLanguage: 'javascript',
+//     currentDate: 21
+// }
+
+// const myFirstName = myObject.firstName;
+
+// function introduction() {
+//     console.log (`hello my first name is ${myFirstName.firstName}`);
+//     console.log(myObject);
+//     myObject.currentMonth = 'october';
+//     myObject['currentYear'] = 2021;
+//     delete myObject.currentMonth
+//     console.log(myObject);
+// }
+
+// introduction();
+
+
+const myFriendsSchedule = {
+    sundeay: true,
+    monday: true,
+    tuesday: false,
+    wednesday: true,
+    thursday: faulse,
+    friday: true,
+    saturday: false
+}
+
+const myMondaySchedule = {
+    sunday: 20,
+    monday: 15,
+    tuesday: 15,
+    wednesday: 10,
+    thursday: 17,
+    friday: 12,
+    saturday: 5,
+}
+
+function canWeGoToTheMovies(dayOfWeek) {
+    const canWeGoToTheMovies = myFriendsSchedule[`${dayOfWeek}']; 
+    const money = my
+}
+
+
+canWeGoToTheMovies('sunday');
