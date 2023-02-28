eb5cd94fa2313ac140ddaccfaef744e0582e0876
LaDonte LeBlanc <donte.11@hotmail.com>
2023-02-28 17:26:38 -0500

    about_me

--- recipe-exercises/javascript_lesson.js/javascript_lesson.js
+++ recipe-exercises/javascript_lesson.js/javascript_lesson.js
@@ -102,3 +102,38 @@ function canWeGoToTheMovies(dayOfWeek) {
 
 
 canWeGoToTheMovies('sunday');
+
+
+Practice 4 Loops 
+
+// Lets check all days of the week using FOR Loop
+Use the same variables from the other exercise
+const myMondaySchedule = {
+    sunday: 20,
+    monday: 15,
+    tuesday: 15,
+    wednesday: 10,
+    thursday: 17,
+    friday: 12,
+    saturday: 5,
+}
+// create a new variable called DAYS_OF_WEEK that is a string array of all the days of the week i.e. ["sunday"]
+// create a function called canWeGoToTheMovies that has not parameters
+// create a variable called whatDaysCanWeGoToTheMovies that is an empty string. i.e. there is nothing inside the open and close quotes
+// output/print the days of the week we are checking. What would we use to do that?
+// i.e. We are checking the days of the week: DAYS_OF_WEEK
+// Create a for loop using the length of DAYS_OF_WEEK as the condition
+// create a variable called dayOfWeek that will have the day of week as we iterate over the array
+// create an if statement for myFriendsSchedule and myMoneySchedule.
+// Using bracket notation lookup the day of the week for both objects, find out if my friend is free AND I have the money
+// If they have the time AND I have the money then we can go! Add that to the whatDaysCanWeGoToTheMovies string
+// ex. "On " + theDateWeWantToGo + "? Yes, lets go to the movies!!"
+// Create an else/if statement to check if my friend has the time OR if I have the money
+// If they have the time OR I have the money then we can go!  Add that to the whatDaysCanWeGoToTheMovies string
+// ex. "On " + theDateWeWantToGo + "? One of us can go to the movies"
+// Create an else statement
+// if NEITHER they have the time AND I don't have the money then we can not go :(
+// ex: "On " + theDateWeWantToGo + "? We can't go to the movies"
+// output/print the variable whatDaysCanWeGoToTheMovies
+// exit the function
+// call the function
